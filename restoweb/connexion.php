f2a7973cb51f798039bdc33b75e164966838d831
theliauanouilh <theliau.anouilh@limayrac>
2026-09-16 14:57:01 +0200

    Modification connexion

--- restoweb/connexion.php
+++ restoweb/connexion.php
@@ -6,11 +6,21 @@
     <title>connexion</title>
 </head>
 <body>
+
+    <h2>Veillez vous connecter à votre compte </h2>
+
+    <p>Vous n'avez pas de compte ? <a href="inscription.php" title="Linktitle">S'inscrire</a></p>
+
     <form>
 
-        <input name="Adresse mail" id="mail" type="text" value="ici" />
-        
+        <label>Adresse mail :</label></br>
+        <input name="Adresse mail" id="mail" type="text" value="" /></br></br>
 
+        <label>Mot de passe:</label></br>
+        <input name="mdp" id="mdp" type="password" size="10" value="" /></br></br>
+
+        <input type="submit" name="submit" value="Se connecter"/>
+        
     </form>
 </body>
 </html>

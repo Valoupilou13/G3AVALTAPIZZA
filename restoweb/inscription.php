a7b5f959d5e6ce10ece7f899b94d55d698c8fa36
adrienmouly <adrien.mouly@limayrac.fr>
2026-09-16 14:48:52 +0200

    Modification page inscription

--- restoweb/inscription.php
+++ restoweb/inscription.php
@@ -15,7 +15,8 @@
     <label for="pseudo" >Pseudo</label>
     <input type="text" id="pseudo" name="pseudo" value="Mon pseudo"/> <br>
     <label for="Mot de passe">Mot de passe</label>
-    <input name="mdp" id="Mot de passe" type="password" size="10" value="" />
+    <input name="mdp" id="Mot de passe" type="password" size="10" value="" /> </br>
+    <input type="submit" name="submit" value="S'inscrire" />
     </p>
     </form>
 </body>
